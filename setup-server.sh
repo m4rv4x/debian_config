a1d0140daca1ccedc6bd3cf2907ff2ae79c40bfb
m4rv4x <m4rv4x@gmail.com>
2023-05-31 14:40:44 +0200

    update

--- setup-server.sh
+++ setup-server.sh
@@ -4,15 +4,15 @@
 sudo apt-get update && sudo apt-get upgrade -y
 
 # Install useful developer packages
-sudo apt-get install build-essential git curl wget zsh -y
+sudo apt-get install build-essential nodejs python3 git curl wget zsh -y
 
 # Install Docker
-sudo apt-get install apt-transport-https ca-certificates gnupg lsb-release docker.io docker-compose -y
-sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates gnupg lsb-release -y
+sudo apt-get install docker.io docker-compose -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
 # Install system admin packages
-sudo apt-get install htop nmap byobu -y
+sudo apt-get install htop nmap byobu curl node speedtest-cli -y
 
 # Install zsh and set as default shell
 sudo apt-get install zsh -y
