af16af88fa0f173b1d69fef387d23666e449576d
marvax <33978801+m4rv4x@users.noreply.github.com>
2023-07-13 17:14:59 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ sudo apt-get update && sudo apt-get upgrade -y
 # Install Latest node.js
 curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - &&\
 
-# Upgrade docker-compose to version 3
+# Install latest docker
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
@@ -15,21 +15,22 @@ sudo groupadd docker
 # Now, you can add the user to the docker user group with the usermod command:
 sudo usermod -aG docker $USER
 
-# Upgrade Ruby to version 3 (or last)
+# Install latest Ruby
 gpg2 --keyserver hkp://keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io -o ~/rvm.sh
 chmod +x ~/rvm.sh
 ./rvm/sh
 rm -r ~/rvm.sh
 
+# Install Github-cli
+wget https://github.com/cli/cli/releases/download/v2.32.0/gh_2.32.0_linux_amd64.deb
+sudo dpkg -i gh_2.32.0_linux_amd64.deb
 
 # Install system admin packages
 sudo apt-get install htop nmap micro netcat byobu curl speedtest-cli gcc g++ make zsh neofetch exa ncdu curl wget  -y
+
 # Install useful developer packages
 sudo apt-get install build-essential nodejs python3 git python3-venvapt-transport-https ca-certificates gnupg lsb-release -y
-# Install some Python packages
-wget https://github.com/cli/cli/releases/download/v2.32.0/gh_2.32.0_linux_amd64.deb
-sudo dpkg -i gh_2.32.0_linux_amd64.deb
 
 # Install zsh and set as default shell
 chsh -s $(which zsh)
