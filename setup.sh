00795ad8cfdf9f215d33089fbb32f27a41a351de
marvax <33978801+m4rv4x@users.noreply.github.com>
2023-07-13 17:05:16 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,10 +5,10 @@ sudo apt-get update && sudo apt-get upgrade -y
 
 # Install Ruby 3
 gpg2 --keyserver hkp://keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-curl -sSL https://get.rvm.io -o rvm.sh
-chmod +x rvm.sh
+curl -sSL https://get.rvm.io -o ~/rvm.sh
+chmod +x ~/rvm.sh
 ./rvm/sh
-rm -r rvm.sh
+rm -r ~/rvm.sh
 
 # Install Latest node.js
 curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - &&\
