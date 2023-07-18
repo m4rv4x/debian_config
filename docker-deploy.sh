f12f29b689d47b92265e6ea9094f181d68286554
dirtyak <dirtyak@mini>
2023-07-18 20:11:43 +0200

    update

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -10,21 +10,39 @@ docker rm $(docker ps -aq)
 docker volume prune
 
 echo "[!] DONE"
-echo "[+] RUNNING NEW CONTAINERS"
-
    # Run each container with the appropriate command
    # docker run -d --name portainer_agent portainer/agent:2.18.4 
    #docker run -d --name transmission -p 86:9091 -p 51413:51413/udp -p 51413:51413/tcp linuxserver/transmission:latest
+echo "[+] RUNNING NEW CONTAINER : PORTAINER"
+docker run -d --name portainer \
+   -p 8000:8000 -p 9443:9443 -p 9000:9000 \
+   --restart=unless-stopped \
+   -v /var/run/docker.sock:/var/run/docker.sock \
+   -v portainer_data:/data portainer/portainer-ce:latest
+echo "[+] RUNNING NEW CONTAINER : NETDATA"
 docker run -d --name netdata netdata/netdata:latest # PORT 19999
-docker run -d --name OpenVPNas -p 943:943 -p 1194:1194/udp -p 9444:9443/tcp linuxserver/openvpn-as:latest
+echo "[+] RUNNING NEW CONTAINER : OPENVPN AS"
+docker run -d --name OpenVPNas -p 943:943 -p 1194:1194/udp -p 9444:9443/tcp \
+   --restart=unless-stopped \
+   linuxserver/openvpn-as:latest
 #docker run -d --name airsonic -p 81:4040 linuxserver/airsonic:latest
    #docker run -d --name fresh-rss -p 82:80 -p 8243:443 linuxserver/freshrss:latest
+echo "[+] RUNNING NEW CONTAINER : GRAV BLOG"
 docker run -d --name grav -p 83:80 -p 32777:443 linuxserver/grav:latest
-docker run -d --name dozzle -p 84:8080 --restart=unless-stopped --volume=/var/run/docker.sock:/var/run/docker.sock amir20/dozzle:latest 
+echo "[+] RUNNING NEW CONTAINER : DOZZLE"
+docker run -d --name dozzle -p 84:8080 \
+   --restart=unless-stopped \
+   --volume=/var/run/docker.sock:/var/run/docker.sock \
+   amir20/dozzle:latest 
+echo "[+] RUNNING NEW CONTAINER : UPTIME KUMA"
 docker run -d --name uptime-kuma -p 85:3001 louislam/uptime-kuma:1 
-docker run -d --name portainer -p 8000:8000 -p 9443:9443 -p 9000:9000 --restart=unless-stopped -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
-docker run -d --name heimdall -p 80:80 -p 8043:443 --restart=unless-stopped -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower heimdall
-docker run -d --name metube -p 8081:8081 -v /path/to/downloads:/hdd/downloads ghcr.io/alexta69/metube
+echo "[+] RUNNING NEW CONTAINER : HEIMDAL"
+docker run -d --name heimdall -p 80:80 -p 8043:443 \
+   --restart=unless-stopped \
+   -v /var/run/docker.sock:/var/run/docker.sock \
+   containrrr/watchtower heimdall
+echo "[+] RUNNING NEW CONTAINER : METUBE"
+docker run -d --name metube -p 8081:8081 -v /hdd/downloads:/downloads ghcr.io/alexta69/metube
 ##################### TRANSMISSION TORRENT #####################
 echo "[+] RUNNING NEW CONTAINER : TRANSMISSION"
 docker run -d \
@@ -41,9 +59,9 @@ docker run -d \
   -p 9091:9091 \
   -p 51413:51413 \
   -p 51413:51413/udp \
-  -v /path/to/data:/hdd/config \
-  -v /path/to/downloads:/hdd/downloads \
-  -v /path/to/watch/folder:/hdd/transmission/watch \
+  -v /hdd/config:/config \
+  -v /hdd/downloads:/downloads \
+  -v /hdd/watch:/watch \
   --restart unless-stopped \
   lscr.io/linuxserver/transmission:latest
 ######################################################################## FRESH RSS
@@ -54,7 +72,7 @@ docker run -d \
   -e PGID=1000 \
   -e TZ=Etc/UTC \
   -p 82:80 \
-  -v /path/to/data:/hdd/config \
+  -v /hdd/config:/config \
   --restart unless-stopped \
   linuxserver/freshrss:latest
 ########################################################################
@@ -64,9 +82,17 @@ docker run -d \
   -e PUID=1000 \
   -e PGID=1000 \
   -p 81:4040 \
-  -v /path/to/config:/hdd/config \
-  -v /path/to/music:/hdd/downloads \
+  -v /hdd/config:/config \
+  -v /hdd/downloads:/music \
   --restart unless-stopped \
   lscr.io/linuxserver/airsonic
+
+##################################################################################
+docker run -d \
+  --name=guacamole \
+  -p 86:8080 \
+  -v /hdd/config:/config \
+  --restart unless-stopped \
+  oznu/guacamole
 
 echo "[!] DONE"
