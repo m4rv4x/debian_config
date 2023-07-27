788a7975baf2b89f58b8287ca62b6b4a29bf3c86
m4rv4x <m4rv4x@gmail.com>
2023-07-27 19:53:43 +0200

    update

--- setup.sh
+++ setup.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-cp ./zshrc/aliases ~/.aliases
 
 # Update and upgrade packages
 sudo apt update && sudo apt upgrade -y
 sudo apt install git curl wget -y
-
-# Install Latest node.js
-curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - &&\
 
 # Install system admin packages
 sudo apt install htop nmap micro zsh exa -y
@@ -17,12 +13,10 @@ sudo apt install speedtest-cli -y
 sudo apt install open-ssl -y
 sudo apt install netcat -y
 sudo apt install gcc g++ make -y
-byobu-ctrl-a screen
-byobu-enable
 
 # Install useful developer packages
-sudo apt install python3-full -y
-sudo apt install python3-pip -y
+sudo apt install python3 -y
+sudo apt install pipx -y
 sudo apt install build-essential nodejs -y
 sudo apt install gnupg lsb-release -y
 
@@ -30,15 +24,11 @@ sudo apt install gnupg lsb-release -y
 chsh -s $(which zsh)
 # Install Oh My Zsh for better zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
+# Add some ZSH CONFIG
+cp ./zshrc/aliases ~/.aliases
+cp ./zshrc/ohmy.zshrc ~/.zshrc
+# Reload
+source .zshrc
 # Clean up
 sudo apt autoremove -y
 sudo apt clean
-
-# Install Github-cli
-wget https://github.com/cli/cli/releases/download/v2.32.0/gh_2.32.0_linux_amd64.deb
-sudo dpkg -i gh_2.32.0_linux_amd64.deb
-rm -r gh_2.32.0_linux_amd64.deb
-# Init GitHub account
-gh auth login
-echo "[!] Install Done"
