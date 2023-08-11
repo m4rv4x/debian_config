55df2f816b6a1aa2afbaac42e29786d31534aad4
m4rv4x <m4rv4x@gmail.com>
2023-08-11 16:07:20 +0200

    fix

--- setup.sh
+++ setup.sh
@@ -3,45 +3,65 @@
 # Update and upgrade packages
 sudo apt update && sudo apt upgrade -y
 
-# Install git if it exists in the packet manager
+# Check if git is installed
 if dpkg -s git &> /dev/null; then
-    sudo apt install git -y
+    echo "Git is already installed"
+else
+    echo "Git is not installed"
 fi
 
 # Install curl if it exists in the packet manager
 if dpkg -s curl &> /dev/null; then
+    echo "Curl is already installed"
+else
     sudo apt install curl -y
 fi
 
 # Install wget if it exists in the packet manager
 if dpkg -s wget &> /dev/null; then
+    echo "Wget is already installed"
+else
     sudo apt install wget -y
 fi
 
 # Install system admin packages
 if dpkg -s htop nmap micro exa byobu ncdu neofetch &> /dev/null; then
+    echo "System admin packages are already installed"
+else
     sudo apt install htop nmap micro exa byobu ncdu neofetch -y
 fi
 if dpkg -s speedtest-cli open-ssl netcat &> /dev/null; then
+    echo "Speedtest-cli, open-ssl, and netcat are already installed"
+else
     sudo apt install speedtest-cli open-ssl netcat -y
 fi
 if dpkg -s gcc g++ make &> /dev/null; then
+    echo "GCC, g++, and make are already installed"
+else
     sudo apt install gcc g++ make -y
 fi
 
 # Install useful developer packages
 if dpkg -s python3 pipx &> /dev/null; then
+    echo "Python3 and pipx are already installed"
+else
     sudo apt install python3 pipx -y
 fi
 if dpkg -s build-essential &> /dev/null; then
+    echo "Build-essential is already installed"
+else
     sudo apt install build-essential -y
 fi
 if dpkg -s gnupg lsb-release &> /dev/null; then
+    echo "Gnupg and lsb-release are already installed"
+else
     sudo apt install gnupg lsb-release -y
 fi
 
 # Install Shell Additions
 if dpkg -s zsh zsh-antigen zsh-autosuggestions zsh-common zsh-syntax-highlighting zsh-theme-powerlevel9k &> /dev/null; then
+    echo "Zsh and shell additions are already installed"
+else
     sudo apt install zsh zsh-antigen zsh-autosuggestions zsh-common zsh-syntax-highlighting zsh-theme-powerlevel9k
 fi
 # Install zsh and set as default shell
